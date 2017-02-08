3d1eccdb80e72005c7257c60e757212afb8667e5
Jason Rosa <jasorosa@ulb.ac.be>
2017-02-08 17:19:04 +0100

    working project for ise added

---
+++ Firmware/ipbus/firmware/example_designs/scripts/setup.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+mkdir ipcore_dir
+ln -s ~/Dropbox/ULB/MA2/Memoire/Vivado/ipbus/ipbus/firmware/example_designs/projects/demo_kc705_basex/ise14/coregen.cgp ipcore_dir/coregen.cgp
+xtclsh ~/Dropbox/ULB/MA2/Memoire/Vivado/ipbus/ipbus/firmware/example_designs/projects/demo_kc705_basex/ise14/setup_project.tcl
+
---
+++ Firmware/project_ipbus_ise/_impact.cmd
@@ -0,0 +1,18 @@
+setMode -bs
+setMode -bs
+setMode -bs
+setMode -bs
+setCable -port auto
+setCable -port auto
+setCable -port auto
+setCable -port auto
+setMode -bs
+setMode -bs
+setMode -bs
+setMode -ss
+setMode -sm
+setMode -hw140
+setMode -spi
+setMode -acecf
+setMode -acempm
+setMode -pff
